a2f37ac94da17a0116081477ac76fb8bf5f8f721
e.lascano <emilas5678@gmail.com>
2022-09-22 10:40:09 -0400

    Create and stub up 5 new pages

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,10 @@
+
+
+function Projects() {
+  return (
+      <h1>About</h1>
+  )
+}
+
+
+export default Projects
---
+++ src/pages/Resume.jsx
@@ -0,0 +1,10 @@
+
+
+function Resume() {
+  return (
+      <h1>About</h1>
+  )
+}
+
+
+export default Resume
