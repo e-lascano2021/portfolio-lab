2d8cb5a68c4ee2bb7fffd3ec02db12943e515642
e.lascano <emilas5678@gmail.com>
2022-09-22 12:43:43 -0400

    show unordered list of project titles

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,15 @@
-
+import { projects } from "../data/projects"
 
 function Projects() {
   return (
-    <h1>Projects</h1>
+    <main>
+      <h1>Projects</h1>
+      <ul>
+        {projects.map(project => 
+          <li> {project.title} </li>
+        )}
+      </ul>
+    </main>
   )
 }
 
