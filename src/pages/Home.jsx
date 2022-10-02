f8a6fc3570470e9eba428c606bd0c6faf14bb5f7
e.lascano <emilas5678@gmail.com>
2022-10-02 13:34:01 -0400

    add home css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ function Home() {
   return (
     <main id="home">
       <h1>Emily Lascano</h1>
-      <img src="https://i.imgur.com/pBDj7Trb.jpg" alt="Emily Lascano"/>
+      <img src="https://i.imgur.com/PX4TLWn.png" alt="Emily Lascano"/>
       <h2>Software Engineer</h2>
     </main>
   )
