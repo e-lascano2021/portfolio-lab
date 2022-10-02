88ad9d0ac389c7ec09fa43abd8ef6a99968ead00
e.lascano <emilas5678@gmail.com>
2022-10-02 14:21:05 -0400

    add Footer to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Resume from './pages/Resume'
 import Projects from './pages/Projects'
 import NavBar from './elements/NavBar'
 import ProjectDetails from './pages/ProjectDetails'
+import Footer from './elements/Footer'
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
         <Route path="projects" element={<Projects/>}/>
         <Route path="projects/:projectDetails" element={<ProjectDetails/>}/>
       </Routes>
+      <Footer/>
     </>
   );
 }
---
+++ src/elements/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer () {
+  return (
+    <div>
+      <ul>
+        <a href="https://github.com/e-lascano2021" ><li>Github</li></a>
+        <a href="https://www.codewars.com/users/e-lascano" ><li>CodeWars</li></a>
+        <a href="https://www.linkedin.com/in/emily-lascano/" ><li>Linkdin</li></a>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
