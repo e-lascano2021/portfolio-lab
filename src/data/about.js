9ae8f4988be88f88d6b65f49cb5beb6d26e5e79e
e.lascano <emilas5678@gmail.com>
2022-10-03 10:40:16 -0400

    make data for about page

---
+++ src/data/about.js
@@ -0,0 +1,27 @@
+const about = {
+  funFacts: [
+    "I love Mystery Games",
+    "I enjoy reading books in first person POV",
+    "I used to twirl fire batons in high school",
+    "I participated in an AFJROTC in high school",
+    "My favorite animal is a horse",
+  ],
+
+  favoriteBooks: [
+    {
+      title: "Angel Burn",
+      author: "L.A. Weatherly"
+    },
+    {
+      title: "The Catastrphic History of You And Me",
+      author: "Jess Rothenberg"
+    },
+    {
+      title: "The Fifth Wave",
+      author: "Rick Yancey"
+    },
+  ]
+
+}
+
+export { about }
