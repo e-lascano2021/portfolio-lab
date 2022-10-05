0d3f994491d7e5c2b8ad53e482d009e11f7290c2
e.lascano <emilas5678@gmail.com>
2022-10-05 11:28:21 -0400

    style about page

--- src/elements/BooksList.jsx
+++ src/elements/BooksList.jsx
@@ -2,7 +2,7 @@ import BookCard from "./BookCard.jsx"
 
 function BooksList(props) {
   return(
-    <div id="facts-list">
+    <div id="books-list">
       <h2>Favoite Books</h2>
       <ul>
         {props.books.map((book, idx )=> 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,12 +5,17 @@ import FunFactsList from "../elements/FunFactsList.jsx"
 function About () {
   return (
     <main id="about">
-      <h1>Meet The Engineer:</h1>
-      <p>paragraph 1</p>
+      <section id="meetTheEngineer">
+        <div>
+          <h1>Meet The Engineer:</h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore sed distinctio sit itaque nesciunt expedita eos neque fuga in, recusandae minus officia, maxime sunt porro illo voluptatibus reiciendis quibusdam reprehenderit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim autem veritatis incidunt aspernatur nisi! Neque non iusto accusamus ut distinctio temporibus dolores in deserunt sint officia, ab beatae rem deleniti.</p>
+        </div>
+      </section>
 
-      <h1>More about Me:</h1>
-      <FunFactsList facts={about.funFacts}/>
-      <BooksList books={about.favoriteBooks} />
+      {/* <section id="moreAboutMe"> */}
+          <FunFactsList facts={about.funFacts}/>
+          <BooksList books={about.favoriteBooks} />
+      {/* </section> */}
     </main>
   )
 }
